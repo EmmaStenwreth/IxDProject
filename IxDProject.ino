9b09f34dc317569e1e230b75b98c4d5756979ff2
EmmaStenwreth <97emst21@gmail.com>
2022-09-29 10:54:57 +0200

    Remove code for accelerometer.

--- IxDProject.ino
+++ IxDProject.ino
@@ -1,10 +1,14 @@
-
+#include <Adafruit_NeoPixel.h>
 #include <Arduino_LSM6DSOX.h>
 
-float Ax, Ay, Az;
-float Gx, Gy, Gz;
+#define LED_PIN 3
+#define LED_COUNT 39
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 int motorPin = 2; // motor transistor connected to pin D2
+
+int red, green, blue;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,64 +17,23 @@ void setup() {
 
   Serial.begin(9600);
 
-  while(!Serial);
-
-  if(!IMU.begin()){
-    Serial.println("Failed to initialize IMU!");
-    while(1);
-  }
-
-  Serial.print("Acceleromeeter sample rate = ");
-  Serial.print(IMU.accelerationSampleRate());
-  Serial.println("Hz");
-  Serial.println();
-
-  Serial.print("Gyroscope sample rate = ");
-  Serial.print(IMU.gyroscopeSampleRate());
-  Serial.println("Hz");
-  Serial.println();
-  
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(IMU.accelerationAvailable()) {
-    IMU.readAcceleration(Ax, Ay, Az);
-
-//    Serial.println("Accelerometer data: ");
-//    Serial.print(Ax);
-//    Serial.print('t');
-//    Serial.println(Ay);
-//    Serial.println('t');
-//    Serial.println(Az);
-//    Serial.println();
-
-//    Serial.println("X: ");
-//    Serial.println(Ax);
-//    Serial.println("Y: ");
-//    Serial.println(Ay);
-//    Serial.println("Z: ");
-//    Serial.println(Az); 
-
-    Serial.println(String(Ax) + "." + String(Ay) + "." + String(Az));
+  for(int i=0; i<4; i++){
+    red = random(255);
+    green = random(255);
+    blue = random(255);
+    strip.setPixelColor(i, red, green, blue);
+    delay(100);
+    strip.clear();
+    delay(100);    
   }
-
-//  if(IMU.gyroscopeAvailable()) {
-//    IMU.readGyroscope(Gx, Gy, Gz);
-//
-//    Serial.println("Gyroscope data: ");
-//    Serial.print(Gx);
-//    Serial.print('t');
-//    Serial.print(Gy);
-//    Serial.print('t');
-//    Serial.print(Gz);
-//    Serial.println();
-//    //Serial.print();
-//  }
-
-  delay(500);
-
+  
   //digitalWrite(motorPin, HIGH);
   //delay(1000);
   //digitalWrite(motorPin, LOW);
