30ff735d3069e04d1833d7806e543313696c7c17
EmmaStenwreth <97emst21@gmail.com>
2022-10-05 10:33:27 +0200

    Add Blutetooth connection

--- IxDProject.ino
+++ IxDProject.ino
@@ -1,29 +1,61 @@
-#include <Adafruit_NeoPixel.h>
 #include <FastLED.h>
+// Library for Bluetooth connection
+#include <ArduinoBLE.h>
+BLEService newService("180A"); // creating the service
+
+BLEUnsignedCharCharacteristic randomReading("2A58", BLERead | BLENotify); // creating the Analog Value characteristic
+BLEByteCharacteristic switchChar("2A57", BLERead | BLEWrite); // creating the LED characteristic
+BLEByteCharacteristic switchChar("2A59", BLERead | BLEWrite); // creating the LED-strip characteristic
+
+// ledPin is for single led indicating bluetooth connection.
+const int ledPin = 2;
+long previousMillis = 0;
 
 #define LED_PIN 3
 #define LED_COUNT 4
-#define BRIGHTNESS 5
+#define BRIGHTNESS 100
 
 CRGB leds[LED_COUNT];
 
 byte r,g,b;
 float brightness;
 
-int motorPin = 2; // motor transistor connected to pin D2
-
 void setup() {
   // put your setup code here, to run once:
 
   //pinMode(motorPin, OUTPUT);
 
-  //Serial.begin(9600);
-
- Serial.begin(9600);
   // init the LED object
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, LED_COUNT);
   // set random seed
   randomSeed(analogRead(0));
+
+ Serial.begin(9600);
+
+  while(!Serial);
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+  pinMode(ledPin, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+
+  //initialize ArduinoBLE library
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy failed!");
+    while (1);
+  }
+
+  BLE.setLocalName("Great Ball of Fire"); //Setting a name that will appear when scanning for Bluetooth® devices
+  BLE.setAdvertisedService(newService);
+
+  newService.addCharacteristic(switchChar); //add characteristics to a service
+  newService.addCharacteristic(randomReading);
+
+  BLE.addService(newService);  // adding the service
+
+  switchChar.writeValue(0); //set initial value for characteristics
+  randomReading.writeValue(0);
+
+  BLE.advertise(); //start advertising the service
+  Serial.println("Bluetooth® device active, waiting for connections...");
 }
 
 void loop() {
@@ -40,6 +72,45 @@ void loop() {
 //    leds[i] = CRGB::Black;
 //    FastLED.show();
 //  }
+
+  BLEDevice central = BLE.central(); // wait for a Bluetooth® Low Energy central
+
+  if (central) {  // if a central is connected to the peripheral
+    Serial.print("Connected to central: ");
+
+    Serial.println(central.address()); // print the central's BT address
+
+    digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
+
+
+
+    while (central.connected()) { // while the central is connected:
+      long currentMillis = millis();
+
+      if (currentMillis - previousMillis >= 200) {
+        previousMillis = currentMillis;
+
+        int randomValue = analogRead(A1);
+        randomReading.writeValue(randomValue);
+
+        if (switchChar.written()) {
+          if (switchChar.value()) {   // any value other than 0
+            Serial.println("LED on");
+            digitalWrite(ledPin, HIGH);         // will turn the LED on
+          } else {                              // a 0 value
+            Serial.println(F("LED off"));
+            digitalWrite(ledPin, LOW);          // will turn the LED off
+          }
+        }
+
+      }
+    }
+
+    digitalWrite(LED_BUILTIN, LOW); // when the central disconnects, turn off the LED
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
+
 
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.delay(1000);
