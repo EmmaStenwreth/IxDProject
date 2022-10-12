946107f9f339d99d58b78490e4aabab25385d402
marcuslidman <113972751+marcuslidman@users.noreply.github.com>
2022-10-12 12:14:57 +0200

    Update IxDProject.ino

--- IxDProject.ino
+++ IxDProject.ino
@@ -4,12 +4,12 @@ void printDetail(uint8_t type, int value);
 
 #include <FastLED.h>
 
-#define LED1 3
+#define LED1 1
 #define LED1_COUNT 4
 #define BRIGHTNESS 5
 
-#define LED2 0
-#define LED2_COUNT 8
+#define LED2 2
+#define LED2_COUNT 6
 
 
 CRGB leds[LED1_COUNT];
@@ -19,7 +19,10 @@ CRGB leds2[LED2_COUNT];
 byte r,g,b;
 float brightness;
 
-int vibration1 = 2;
+int vibration1 = A1;
+int vibration2 = A2;
+int vibration3 = A3;
+int vibration4 = A4;
 
 void setup() {
   // put your setup code here, to run once:
@@ -31,10 +34,12 @@ void setup() {
   pinMode(LED2, OUTPUT);
 
   pinMode(vibration1, OUTPUT);
+  pinMode(vibration2, OUTPUT);
   //analogWrite(vibration1, 0);// This code makes it vibrate all the time :(
 
   Serial1.begin(9600);
-  Serial.begin(115200);
+  //Serial.begin(115200);
+  Serial.begin(9600); // GD <2022-10-11 Tue 18:09>
 
     Serial.println();
     Serial.println(F("DFRobot DFPlayer Mini Demo"));
@@ -72,140 +77,190 @@ void playSong() {
   FastLED.clear();
   FastLED.show();
 
-  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+  myDFPlayer.volume(1);  //Set volume value. From 0 to 30
   myDFPlayer.play(1);  //Play the first mp3
   delay(588*7);
 
   FastLED.setBrightness(BRIGHTNESS);
     
-  vibrateLow();  
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
-  fill_solid(leds2, LED2_COUNT, CRGB::Red);
-  vibrationAndLedDelay();
+  //vibrateLow();  
+  //fill_solid(leds, LED1_COUNT, CRGB::Red);
+  //fill_solid(leds2, LED2_COUNT, CRGB::Red);
+  right();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Blue);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Red);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Blue);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Red);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Blue);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Red);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Blue);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
 
   fill_solid(leds, LED1_COUNT, CRGB::Yellow);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Green);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Yellow);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Green);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Yellow);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Green);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Yellow);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   fill_solid(leds, LED1_COUNT, CRGB::Green);
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   
   leds[0] = CRGB:: Yellow;
   leds[1] = CRGB:: Red;
   leds[2] = CRGB:: Green;
   leds[3] = CRGB:: Blue;
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   leds[0] = CRGB:: Blue;
   leds[1] = CRGB:: Green;
   leds[2] = CRGB:: Red;
   leds[3] = CRGB:: Yellow;
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   leds[0] = CRGB:: Yellow;
   leds[1] = CRGB:: Red;
   leds[2] = CRGB:: Green;
   leds[3] = CRGB:: Blue;
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   leds[0] = CRGB:: Blue;
   leds[1] = CRGB:: Green;
   leds[2] = CRGB:: Red;
   leds[3] = CRGB:: Yellow;
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   leds[0] = CRGB:: Yellow;
   leds[1] = CRGB:: Red;
   leds[2] = CRGB:: Green;
   leds[3] = CRGB:: Blue;
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   leds[0] = CRGB:: Blue;
   leds[1] = CRGB:: Green;
   leds[2] = CRGB:: Red;
   leds[3] = CRGB:: Yellow;
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   leds[0] = CRGB:: Yellow;
   leds[1] = CRGB:: Red;
   leds[2] = CRGB:: Green;
   leds[3] = CRGB:: Blue;
-  vibrationAndLedDelay();
+  vibrationUpAndLedDelay();
   leds[0] = CRGB:: Blue;
   leds[1] = CRGB:: Green;
   leds[2] = CRGB:: Red;
   leds[3] = CRGB:: Yellow;
-  vibrationAndLedDelay();
-  vibrateLow();
+  vibrationUpAndLedDelay();
 
   stopSong();
   
   
 }
 
-void vibrateHigh() {
-  digitalWrite(vibration1, HIGH); //vibrate 
+void vibrationUpAndLedDelay() {
+  digitalWrite(vibration2, HIGH); 
+  FastLED.delay(588);
+  digitalWrite(vibration1, HIGH); 
+  digitalWrite(vibration2, LOW);
+  FastLED.delay(588);
+  digitalWrite(vibration1, LOW);
 }
 
-void vibrateLow() {
-  digitalWrite(vibration1, LOW); //dont vibrate
+void vibrationDownAndLedDelay() {
+  digitalWrite(vibration4, HIGH);
+  digitalWrite(vibration3, HIGH);  
+  FastLED.delay(588);
+  digitalWrite(vibration3, LOW);
+  digitalWrite(vibration4, LOW);
+  FastLED.delay(588);
 }
 
-void vibrationAndLedDelay() {
+void vibrationLeftAndLedDelay() {
+  digitalWrite(vibration2, HIGH);
+  digitalWrite(vibration3, HIGH);  
   FastLED.delay(588);
-  vibrateHigh();
+  digitalWrite(vibration2, LOW);
+  digitalWrite(vibration3, LOW);
   FastLED.delay(588);
-  vibrateLow();
 }
+
+void vibrationRightAndLedDelay() {
+  digitalWrite(vibration4, HIGH); 
+  FastLED.delay(588);
+  digitalWrite(vibration1, HIGH); 
+  digitalWrite(vibration4, LOW);
+  FastLED.delay(588);
+  digitalWrite(vibration1, LOW);
+}
+
+void vibrationLeftUpAndLedDelay() {
+  digitalWrite(vibration2, HIGH); 
+  FastLED.delay(588);
+  digitalWrite(vibration2, LOW);
+  FastLED.delay(588);
+}
+
+void vibrationLeftDownAndLedDelay() {
+  digitalWrite(vibration3, HIGH); 
+  FastLED.delay(588);
+  digitalWrite(vibration3, LOW);
+  FastLED.delay(588);
+}
+
+void vibrationRightUpAndLedDelay() {
+  FastLED.delay(588);
+  digitalWrite(vibration1, HIGH); 
+  FastLED.delay(588);
+  digitalWrite(vibration1, LOW);
+}
+
+void vibrationRightDownAndLedDelay() {
+  digitalWrite(vibration4, HIGH); 
+  FastLED.delay(588);
+  digitalWrite(vibration4, LOW);
+  FastLED.delay(588);
+}
+
 
 //LED-Strip combinations
 void leftUp() {
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
+  fill_solid(leds, LED1_COUNT, CRGB::Green);
 }
 
 void leftDown() {
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
+  fill_solid(leds, LED1_COUNT, CRGB::Blue);
 }
 
 void rightUp() {
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
+  fill_solid(leds2, LED2_COUNT, CRGB::Orange);
 }
 
 void rightDown() {
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
+  fill_solid(leds, LED1_COUNT, CRGB::Pink);
 }
 
 void up() {
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
-  fill_solid(leds2, LED2_COUNT, CRGB::Red);
+  fill_solid(leds, LED1_COUNT, CRGB::Yellow);
+  fill_solid(leds2, LED2_COUNT, CRGB::Yellow);
 }
 
 void down() {
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
-  fill_solid(leds2, LED2_COUNT, CRGB::Red);
+  fill_solid(leds, LED1_COUNT, CRGB::Blue);
+  fill_solid(leds2, LED2_COUNT, CRGB::Blue);
 }
 
 void left() {
-  fill_solid(leds, LED1_COUNT, CRGB::Red);
-  fill_solid(leds2, LED2_COUNT, CRGB::Red);
+  fill_solid(leds, LED1_COUNT, CRGB::Green);
+  fill_solid(leds2, LED2_COUNT, CRGB::Green);
 }
 
 void right() {
